b001d01357f9f81f91b8b2e1e42c3a92254683fe
AngadAhluwalia <angadahluwalia2005@gmail.com>
2026-06-26 06:36:18 -0400

    made changes to home

--- src/section/Home.jsx
+++ src/section/Home.jsx
@@ -1,13 +1,14 @@
-// ...existing code...
-import React from 'react'
-
-const Home = () => {
-  return (
-   <div className='w-full h-screen'>
-    Home
-   </div>
+import ParticlesBackground from "../components/ParticlesBackground"
+export default function Home(){
+  return(
+    <section id="home" className="w-full h-screen rellative bg-black overflow-hidden ">
+      <ParticlesBackground/>
+      <div className="absolute isnet-0">
+        <div className="absolute -top-32 -left-32 w[70vw]sm:w-[z-500vw] md:w-[40vw]"></div>
+        <div></div>
+      
+    </div>
+    </section>
+    
   )
 }
-
-export default Home
-// ...existing code...
