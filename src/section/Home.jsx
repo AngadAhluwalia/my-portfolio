b5872dc9c8e07c0af2aefcbdaec69b25991e1e01
AngadAhluwalia <angadahluwalia2005@gmail.com>
2026-07-01 06:16:52 -0400

    added linkedin and github

--- src/section/Home.jsx
+++ src/section/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from "react"
 import ParticlesBackground from "../components/ParticlesBackground"
-import { motion } from "framer-motion"
+import { motion, scale } from "framer-motion"
 import { FaLinkedin, FaGithub } from "react-icons/fa6"
 
 const socials =[
@@ -8,6 +8,19 @@ const socials =[
   {Icon :FaGithub,label:"GitHub", href:"https://github.com/AngadAhluwalia"}
   
 ]
+const glowVariants = {
+  initial: {
+    scale: 1, y:0, filter: "drop-shadow(0 0 0 rgba(0, 0, 0, 0))",
+  },
+ hover:{
+  scale: 1.2, y:-3, filter: "drop-shadow(0 0 6px rgba(13,88, 204, 0.9))drop-shadow(0 0 8px rgba(16,185, 129, 0.8))",
+  transition: { type : "spring", stiffness: 300, damping: 15 }},
+  tap: {
+    scale: 0.95, y:0, transition:{duration: 0.08},
+  }
+ }
+  
+
 
 export default function Home() {
   const roles = useMemo(() => ["Web Developer", "Software Developer"], [])
@@ -75,7 +88,7 @@ export default function Home() {
               animate={{ opacity: 1, y: 0 }}
               transition={{ delay: 0.4, duration: 0.8 }}
               >a passionate developer with expertise in building modern web applications.</motion.p>
-              <div className="mt-10 flex flex-wrap items-center justify-center lg:justify-start gap-6"
+              <motion.div className="mt-10 flex flex-wrap items-center justify-center lg:justify-start gap-6"
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ delay: 0.8, duration: 0.8 }}
@@ -85,8 +98,9 @@ export default function Home() {
                 <a href="/Angad_Ahluwalia.pdf"
                 download="Angad_Ahluwalia.pdf"
                 className="px-6 py-3 bg-white  text-lg font-medium text-black rounded-full shadow-lg hover:scale-105 transition-all">My resume</a>
-              </div>
-            
+              </motion.div>
+              <div className="mt-10 flex-gap-5 text=2xl md:text-3xl justify-center lg:justify-start">{socials.map(({Icon , label, href})=>(<motion.a href={href} whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} className="flex items-center gap-2 text-white hover:underline"><Icon className="w-5 h-5" /><span>{label}</span></motion.a>))}</div>
+
           </div>
         </div>
       </div>
