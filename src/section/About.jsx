afab56079136466543cd1df092f9e72ca2ba410b
AngadAhluwalia <angadahluwalia2005@gmail.com>
2026-07-02 04:46:54 -0400

    setup about section

--- src/section/About.jsx
+++ src/section/About.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
 
-const About = () => {
+
+
+
+export default function About() {
+  const glows=[
+    "-top-10 -left-10 w-[360px] h-[360px] opacity-20 blur-[120px] ",
+    "-bottom-0 -right-10 w-[420px] h-[420px] opacity-15 blur-[140px] delay-300 ","top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-[220px] h-[220px] opacity-10 blur-[100px] "
+  ]
   return (
-    <div className='w-full h-screen'>
-      About Section
-    </div>
-  )
-}
+    <section id="about" className="w-full min-h-screen relative items-center justify-center bg-black text-white overflow-hidden flex">
+      <div>
+        {glows.map((c,i)=>(
+          <div key={i} className={`absolute rounded-full bg-gradient-to-r from-[#302b63] via-[#00bf8f] to-[#1cd8d2] animate-pulse ${c}`}></div>
+        ))}
+      </div>
 
-export default About
+
+
+
+    </section>
+  )}
+    
